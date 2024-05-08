2dd80b0b626a5cf79c578e564c26f06f6fe05df5
YachaTree <dnftksghkfkd@icloud.com>
2024-05-08 19:08:08 +0900

    item 삭제기능 추가

--- src/main/java/com/tree/shop/item/ItemController.java
+++ src/main/java/com/tree/shop/item/ItemController.java
@@ -1,6 +1,7 @@
 package com.tree.shop.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,32 +21,65 @@ public class ItemController {
         this.itemService = itemService;
     }
 
+    /**
+     * 상품목록 페이지
+     * @param model
+     * @return
+     */
     @GetMapping("/list")
     String list(Model model) {
 
         List<Item> result = itemRepository.findAll();
+        //Html에 데이터 보내려면 model.addAttribute("데이터이름", 데이터 )
         model.addAttribute("items", result);
         var a = new Item();
         System.out.println(a);
         return "list.html";
     }
 
+    /**
+     * 상품추가 페이지
+     * @return
+     */
     @GetMapping("/write")
     String write() {
-
 
         return "write.html";
     }
 
-    @PostMapping("/add")
+
+    /**
+     * 상품추가
+     * @param title
+     * @param price
+     * @return
+     */
+    @PostMapping("/add") // URL 작명시 명사가 좋음
     String addPost(String title, Long price) {
-       itemService.saveItem(title, price);
+        /*
+        @RequestParam(name = "title") 붙이면 되는데 생략가능
+         ajax로 데이터전송하면 @RequestBody 써야 출력가능
+        */
+        itemService.saveItem(title, price);
         return "redirect:/list";
     }
 
-    @GetMapping ("/detail/{id}")
-    String detail(@PathVariable Long id, Model model) {
+//    @PostMapping("/add")
+//    String addPost(@ModelAttribute Item item) {
+//        itemRepository.save(item);
+//        return "redirect:/list";
+//    }
 
+
+    /**
+     * 상세페이지
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping ("/detail/{id}") //@PathVariable은 URL 경로의 일부를 메소드의 파라미터로 추출할 때 사용
+    String detail(@PathVariable Long id, Model model) {
+        //Optional은 해당값이 null값일 수도 있고 아닐수도 있다는 타입이다.
         Optional<Item> result = itemRepository.findById(id);
         if (result.isPresent()) {
             model.addAttribute("data", result.get());
@@ -55,6 +89,12 @@ public class ItemController {
         }
     }
 
+    /**
+     * 상품수정 페이지
+     * @param model
+     * @param id
+     * @return
+     */
     @GetMapping("/edit/{id}")
     String edit(Model model, @PathVariable Long id) {
         Optional<Item> result = itemRepository.findById(id);
@@ -67,24 +107,25 @@ public class ItemController {
 
     }
 
+    /**
+     * 상품수정
+     * @param title
+     * @param price
+     * @param id
+     * @return
+     */
     @PostMapping("/edit")
     String editPost(@RequestParam String title,@RequestParam Long price, @RequestParam Long id) {
 
-        Item item = new Item();
-        item.setId(id);
-        item.setTitle(title);
-        item.setPrice(price);
-
-
-        itemRepository.save(item);
+      itemService.editItem(title, price, id);
         return "redirect:/list";
     }
 
-//    @DeleteMapping("/delete")
-//    String deletePost() {
-//
-//        itemRepository.delete();
-//    }
+    @DeleteMapping("/delete")
+    ResponseEntity<String> deleteItem(@RequestParam Long id) {
+        itemService.deleteItem(id);
+        return ResponseEntity.status(200).body("삭제완료");
+    }
 
 
 }
--- src/main/java/com/tree/shop/item/ItemService.java
+++ src/main/java/com/tree/shop/item/ItemService.java
@@ -1,18 +1,35 @@
 package com.tree.shop.item;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
 
     public void saveItem(String title, Long price) {
         Item item = new Item();
         item.setTitle(title);
         item.setPrice(price);
         itemRepository.save(item);
+    }
+
+    public void editItem(String title, Long price, Long id) {
+        Item item = new Item();
+        item.setId(id);
+        item.setTitle(title);
+        item.setPrice(price);
+        itemRepository.save(item);
+    }
+
+    public void deleteItem(Long id) {
+        itemRepository.deleteById(id);
     }
 }
